0ba1d930894002e05f0b948b0d2c0a3d17d247f6
Willis Shek <willis.shek@gmail.com>
2022-02-14 16:03:23 +1100

    Bootcamp & Courses

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongooose = require("mongoose");
+
+const connectDB = async () => {
+	const conn = await mongooose.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
+
+	console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+};
+
+module.exports = connectDB;
