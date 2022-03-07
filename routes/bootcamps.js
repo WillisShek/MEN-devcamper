b0c09d40a26324783776728f9c307b9cd9328244
Willis Shek <willis.shek@gmail.com>
2022-03-07 16:24:06 +1100

    Add CRUD functionality to users (admin only)

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -10,14 +10,14 @@ const {
 } = require("../controllers/bootcamps");
 
 const Bootcamp = require("../models/Bootcamp");
+
 const advancedResults = require("../middleware/advancedResults");
+const { protect, authorize } = require("../middleware/auth");
 
 // Include other resource routers
 const courseRouter = require("./courses");
 
 const router = express.Router();
-
-const { protect, authorize } = require("../middleware/auth");
 
 // Re-route into other resource routers
 router.use("/:bootcampId/courses", courseRouter);
