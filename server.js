0ba4675171c217f89e73b9d3029f4d4dc8030564
Willis Shek <willis.shek@gmail.com>
2022-03-11 11:53:32 +1100

    Add helmet and xss-clean

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
 const fileUpload = require("express-fileupload");
-const mongoSanitize = require("express-mongo-sanitize");
 const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
 const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 
@@ -40,6 +42,12 @@ app.use(fileUpload());
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
